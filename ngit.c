d1bd5d07ff0fafae612c771203b06c72c7012a8e
Saleh-kgh <tavilisaleh@gmail.com>
2024-01-21 00:19:16 +0330

    added ngit hidden dir func

--- ngit.c
+++ ngit.c
@@ -1,50 +1,56 @@
 #include <stdio.h>
-#include "syntaxErrorRecognizer.h"
 #include <string.h>
+#include <windows.h>
+#include "syntaxErrorRecognizer.h"
+#include "ngitHiddenDir.h"
+
 int main(int argc, char *argv[]) {
     if(strcmp(argv[1], "config")==0) {
-        if(strcmp(argv[2], "user.name")==0 || strcmp(argv[3], "user.name")==0) userInfoSER(argc, argv);
-        else if(strcmp(argv[2], "user.email")==0 || strcmp(argv[3], "user.email")==0) userInfoSER(argc, argv); 
+        if(strcmp(argv[2], "user.name")==0 || strcmp(argv[3], "user.name")==0) if(userInfoSER(argc, argv)==0) return 0;
+        else if(strcmp(argv[2], "user.email")==0 || strcmp(argv[3], "user.email")==0) if(userInfoSER(argc, argv)==0) return 0; 
         char alias_spell[6]="alais."; 
         for(int i=0; i<6; i++) {
             if(argv[3][i]!=alias_spell[i]) {
                 break;
             }
-            if(i==5) aliasSER(argc, argv);
+            if(i==5)
+                if(aliasSER(argc, argv)==0) return 0;
         }    
     }
     else if(strcmp(argv[1], "init")==0) {
-        initSER(argc, argv);
+        if(initSER(argc, argv)==0) return 0;
+        int dirState = makeHiddenNgitDir();
     }
     else if(strcmp(argv[1], "add")==0) {
-        addSER(argc, argv);
+        if(addSER(argc, argv)==0) return 0;
     }
     else if(strcmp(argv[1], "status")==0) {
-        statusSER(argc, argv);
+        if(statusSER(argc, argv)==0) return 0;
     }
     else if(strcmp(argv[1], "commit")==0) {
-        commitSER(argc, argv);
+        if(commitSER(argc, argv)==0) return 0;
     }
     else if(strcmp(argv[1], "set")==0) {
-        setSER(argc, argv);
+        if(setSER(argc, argv)==0) return 0;
     }
     else if(strcmp(argv[1], "replace")==0) {
-        replaceSER(argc, argv);
+        if(replaceSER(argc, argv)==0) return 0;
     }
     else if(strcmp(argv[1], "remove")==0) {
-        removeSER(argc, argv);
+        if(removeSER(argc, argv)==0) return 0;
     }
     else if(strcmp(argv[1], "log")==0) {
-        logSER(argc, argv);
+        if(logSER(argc, argv)==0) return 0;
     }
     else if(strcmp(argv[1], "branch")==0) {
-        branchSER(argc, argv);
+        if(branchSER(argc, argv)==0) return 0;
     }
     else if(strcmp(argv[1], "checkout")==0) {
-        checkoutSER(argc, argv);
+        if(checkoutSER(argc, argv)==0) return 0;
     }
     else {
         printf("Invalid command due to misspell or extra words!");
     }
+
     return 0;
 }
---
+++ ngitHiddenDir.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <windows.h>
+#include "syntaxErrorRecognizer.h"
+
+int makeHiddenNgitDir() {
+    char *folderName = "ngit";
+    char currentDirectory[MAX_PATH];
+    int returnValue=0;
+    if (GetCurrentDirectory(MAX_PATH, currentDirectory) != 0) {
+        strcat(currentDirectory, "\\");
+        strcat(currentDirectory, folderName);
+        if (CreateDirectory(currentDirectory, NULL) || ERROR_ALREADY_EXISTS == GetLastError()) {
+            if (GetLastError() == ERROR_ALREADY_EXISTS) {
+                printf("directory is already set as a ngit repository.");
+                returnValue=1;
+            } 
+            if (SetFileAttributes(currentDirectory, FILE_ATTRIBUTE_HIDDEN)) {
+                printf("directory is succesfully initialized as a ngit repository.\n");
+            } 
+        } 
+        else {
+            printf("systemic error creating repository.");
+            return 0;
+        }
+    }
+    if(returnValue==1) return 2;
+    return 1;
+}
