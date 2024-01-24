bcd47c161eafbd23c1205301e24c791c24909fe1
Saleh-kgh <tavilisaleh@gmail.com>
2024-01-24 16:31:20 +0330

    added lister for files as well, max depth file added too

--- listofcontents.c
+++ listofcontents.c
@@ -83,6 +83,94 @@ void listDirectories(int isOld) {
         if(subDirsCount<1) {
             break; 
         }
+    } 
+    repoPath[strcspn(repoPath, "\n")] = '\0';
+    strcat(repoPath, "\\ngit\\info\\maxDepth.txt");
+    FILE* maxDepthfile=fopen(repoPath, "w");
+    fprintf(maxDepthfile, "%d", dirDepth-1);
+    fclose(maxDepthfile);  
+}
+
+void listFiles(int isOld) {
+    int dirDepth=0;
+    char outputPath[MAX_PATH];
+    GetCurrentDirectory(MAX_PATH,outputPath);
+    char repoPath[MAX_PATH];
+    FILE* reposfile=fopen("d:\\ANGP\\ngit-project\\repositories.txt","r");
+    while(fgets(repoPath, sizeof(repoPath), reposfile)!= NULL) {
+        char* result = strstr(outputPath, repoPath);
+        if(result!=NULL) break;
+    }
+    fclose(reposfile);
+    int maxDepth=0;
+    char maxDepthaddress[MAX_PATH];
+    repoPath[strcspn(repoPath, "\n")] = '\0';
+    sprintf(maxDepthaddress, "%s\\ngit\\info\\maxDepth.txt", repoPath);
+    FILE* maxDepthfile=fopen(maxDepthaddress, "r");
+    fscanf(maxDepthfile, "%d", &maxDepth);
+    fclose(maxDepthfile);
+    strcpy(outputPath, repoPath);
+    outputPath[strcspn(outputPath, "\n")] = '\0';
+    strcat(outputPath, "\\ngit\\info\\contents");
+    char outputfilepath[MAX_PATH];
+    char extralstpath[MAX_PATH];
+    while(1) {
+        sprintf(outputfilepath,"%s\\%d", outputPath, dirDepth);
+        //CreateDirectory(outputfilepath, NULL);
+        //strcpy(extralstpath, outputfilepath);
+        switch (isOld) {
+            case 1 :
+                strcat(outputfilepath, "\\oldfiles.txt");
+                break;
+            case 0 :
+                strcat(outputfilepath, "\\newfiles.txt");
+                break;
+        }
+        FILE* outputfile = fopen(outputfilepath, "w");
+        struct dirent *entry;
+        int subDirsCount=0;
+        if(dirDepth==0) {
+            char dirPath[MAX_PATH];
+            strcpy(dirPath, repoPath);
+            dirPath[strcspn(dirPath, "\n")] = '\0';
+            DIR* dirptr = opendir(dirPath);
+            while ((entry = readdir(dirptr)) != NULL) {
+                if (entry->d_type == DT_REG) {
+                    fprintf(outputfile, "%s\\%s\n", dirPath, entry->d_name);
+                }
+            }
+            closedir(dirptr);
+        }
+        else {
+            char dirPath[MAX_PATH];
+            char Dirsfile[MAX_PATH];
+            sprintf(Dirsfile,"%s\\%d", outputPath, dirDepth-1);
+            switch (isOld) {
+                case 1 :
+                    strcat(Dirsfile, "\\oldDirs.txt");
+                    break;
+                case 0 :
+                    strcat(Dirsfile, "\\newDirs.txt");
+                    break;
+            }
+            FILE* dirsfileptr=fopen(Dirsfile, "r");
+            while(fgets(dirPath, sizeof(dirPath), dirsfileptr)) {
+                dirPath[strcspn(dirPath, "\n")] = '\0';
+                DIR* dirptr = opendir(dirPath);
+                while ((entry = readdir(dirptr)) != NULL) {
+                    if (entry->d_type == DT_REG) {
+                        fprintf(outputfile, "%s\\%s\n", dirPath, entry->d_name);
+                    }
+                }
+                closedir(dirptr);
+            }
+            fclose(dirsfileptr);           
+        }
+        fclose(outputfile);
+        dirDepth++;
+        if(maxDepth+1==dirDepth) {
+            break; 
+        }
     }   
 }
 
--- listofcontents.h
+++ listofcontents.h
@@ -2,5 +2,5 @@
 #define LISTOFCONTENTS_H
 
 void listDirectories(int isOld);
-
+void listFiles(int isOld);
 #endif
